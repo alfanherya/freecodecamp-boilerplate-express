459fdc2f81601c0d031f09ddf6cd5263667c3238
alfan <alfanherya@gmail.com>
2023-06-22 06:39:52 +0700

    serve json on a specific route

--- myApp.js
+++ myApp.js
@@ -4,6 +4,11 @@ let app = express();
 
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 app.use("/public", express.static(__dirname + "/public"));
+app.get("/json", (req, res) => {
+    res.json({
+        message:"   Hello json"
+    });
+});
 
 module.exports = app;
 
