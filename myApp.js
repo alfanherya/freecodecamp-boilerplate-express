000d79630917c97f7ddeb4720969b32c0890b5fb
alfanherya <alfanherya@gmail.com>
2023-06-28 22:41:40 +0700

    implement root middleware success

--- myApp.js
+++ myApp.js
@@ -2,6 +2,12 @@ require('dotenv').config()
 let express = require('express');
 let app = express();
 
+app.use((req, res, next) => {
+    let string = `${req.method} ${req.path} - ${req.ip}`;
+    console.log(string);
+
+    next();
+});
 
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 app.use("/public", express.static(__dirname + "/public"));
@@ -11,12 +17,7 @@ app.get("/json", (req, res) => {
     res.json(json);
 });
 
-app.use((req, res, next) => {
-    let string = `{req.method} ${req.path}-${req.ip}`
-    console.log(string)
 
-    next();
-})
 
 module.exports = app;
 
